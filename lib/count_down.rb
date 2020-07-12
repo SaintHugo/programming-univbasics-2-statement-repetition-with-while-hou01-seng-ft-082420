d1e7960670535678b26f8549359a79c6c5934dd2
Hugo Rodrigez <hugor37@gmail.com>
2020-07-12 17:42:39 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,7 +1,7 @@
-emagic_exit_number = 7
-count = 0
-while count < 10 do
+emagic_exit_number = 0
+count = 10
+while count > 0 do
   break if count == magic_exit_number
-  puts "I am the #{count}, I love to count!" # Work
+  puts "Happy New Year!" # Work
   count = count + 1
 end
